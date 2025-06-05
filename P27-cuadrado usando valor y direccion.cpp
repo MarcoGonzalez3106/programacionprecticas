0faea63e8fdbed4b08d6c9fdafe12a84fd70ff5c
MarcoGonzalez3106 <marcoalexisgonzalezmejia2@gmail.com>
2025-06-04 23:40:52 -0600

    Add files via upload

---
+++ P27-cuadrado usando valor y direccion.cpp
@@ -0,0 +1,38 @@
+//Marco Alexis Gonzalez Mejia
+/*Programa de ejemplo que usa una funcion la cual recibe los 
+parametros por referencia, y una que los recibe por valor. Ambas
+obtienen el cuadrado del argumento. Cuando es por referencia se 
+recibe la direccion de la variable y los cambios que se hacen dentro
+de la funcion se mantienen fuera de ella; cuando es por valor, se 
+recibe una copia de la vaariable, las modificaciones que se hagan a
+ese parametro dentro de la funcion no se veran reflejados fuera de ella.*/
+#include <iostream>
+using namespace std;
+void cuad1(int x);//copia el valor
+void cuad2(int &x);//copia la direccion
+int main()
+{
+	int x;
+	cout<<"dame un valor para elevar el cuadrado ";
+	cin>>x;
+	cout<<"x="<<x<<"antes de enviarla a la funcion 1";
+	system("PAUSE");
+	cuad1(x);
+	cout<<"\nx="<<x<<" despues de enviarla a la funcion 1";
+	system("PAUSE");
+	cuad2(x);
+	cout<<"\nx="<<x<<" despues de enviarla a la funcion 2";
+	return 0;
+}
+void cuad1(int x)
+{
+	x=x*x;
+	cout<<"\nx="<<x<<" DENTRO de la funcion 1";
+	system("PAUSE");
+}
+void cuad2(int &x)
+{
+	x=x*x;
+	cout<<"\nx="<<x<<" DENTRO de la funcion 2";
+	system("PAUSE");
+}
